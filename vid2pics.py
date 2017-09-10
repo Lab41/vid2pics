614432ad4c179f289e32a3f4b49a47ed63a55737
dgrossman <dgrossman@xyxxl.com>
2017-09-10 09:57:35 -0700

    autopep8

--- vid2pics.py
+++ vid2pics.py
@@ -4,7 +4,7 @@ import os
 import sys
 
 
-def find_work(topdir,filetypes):
+def find_work(topdir, filetypes):
     retval = []
     for dirpath, dirnames, files in os.walk(topdir):
         for name in files:
@@ -13,28 +13,30 @@ def find_work(topdir,filetypes):
     return retval
 
 
-def write_frame(img,work_item, outdir, frame_number):
-    file_name = os.path.join(outdir,'{0}-{1:06}.png'.format(work_item,frame_number)) 
-    cv2.imwrite(file_name,img)
+def write_frame(img, work_item, outdir, frame_number):
+    file_name = os.path.join(
+        outdir, '{0}-{1:06}.png'.format(work_item, frame_number))
+    cv2.imwrite(file_name, img)
 
-def process(in_dir,out_dir):
+
+def process(in_dir, out_dir):
     print('Examining input dir for work')
-    work_items = find_work(in_dir,['avi','mov','mp4'])
-    print ('Found {0} work items'.format(len(work_items)))
+    work_items = find_work(in_dir, ['avi', 'mov', 'mp4'])
+    print('Found {0} work items'.format(len(work_items)))
     for work_item in work_items:
         print('Working on: {0}'.format(work_item))
         frame_num = 0
-        cap = cv2.VideoCapture(os.path.join(in_dir,work_item))
+        cap = cv2.VideoCapture(os.path.join(in_dir, work_item))
         while (cap.isOpened()):
-            ret,frame = cap.read()
-            print('frame: {0} ret:{1}'.format(frame_num,ret)) 
-            if ret == True:
-                write_frame(frame,work_item,out_dir,frame_num)
-               
-            frame_num += 1 
-            
+            ret, frame = cap.read()
+            print('frame: {0} ret:{1}'.format(frame_num, ret))
+            if ret:
+                write_frame(frame, work_item, out_dir, frame_num)
+
+            frame_num += 1
+
 
 if __name__ == "__main__":
     in_dir = sys.argv[1]
     out_dir = sys.argv[2]
-    process(in_dir,out_dir)
+    process(in_dir, out_dir)
